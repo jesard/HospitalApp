f37b0be2ccf98e1ea0c732f01ed4c26463824672
Pavel Dvorkin <pdvorkin@lineate.com>
2020-04-25 08:53:34 +0600

    code review

--- src/main/java/net/thumbtack/school/hospital/model/DaySchedule.java
+++ src/main/java/net/thumbtack/school/hospital/model/DaySchedule.java
@@ -7,6 +7,14 @@ import java.util.Map;
 public class DaySchedule {
 
     private LocalDate date;
+    // REVU идея не очень хорошая. Получается, что в этом классе одно поле для доктора, а другое для пациента
+    // так делать не надо
+    // варианты правильного решения
+    // 1. Просто свой класс для доктора и свой для пациента
+    // 2. abstract class AbstractDaySchedule и от него DoctorDaySchedule и PatientDaySchedule
+    // 3. Нужен ли для пациента Map - вопрос спорный. Из идейных соображений тут, конечно Map
+    // но число элементов в нем настолько мало, что вполне можно и List с полным перебором - это никак медленее не будет
+    // да и вообще, подумайте - а нужно ли такое пациенту. Это же DaySchedule. У врача есть дневное расписание, а пациенту оно зачем ?
     private List<Slot> slotSchedule; //for Doctor
     private Map<Doctor, Slot> ticketSchedule; //for Patient
 
